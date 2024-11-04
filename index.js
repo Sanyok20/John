105b24a0883f36ce98932f81bf26cdd5f754a2bd
Sanyok20 <sanyok2009kor@gmail.com>
2024-11-04 19:44:10 +0200

    додав стрілки прокрутки

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ let works = document.querySelectorAll(".work")
 let nav = document.querySelector(".nav")
 let blog = document.querySelector(".blog")
 let burgerIcon = document.querySelector(".burger-icon")
-
+let arrowUp = document.querySelector(".arrow-up")
+let arrowDown = document.querySelector(".arrow-down")
+let arrows = document.querySelector(".arrows")
 //show menu
 burgerIcon.addEventListener("click", showMenu)
 function showMenu(){
@@ -23,12 +25,30 @@ function showDetails(){
 }
 let posts = document.querySelectorAll(".post")
 console.log(posts)
-blog.forEach(item => 
-    item.addEventListener("click", showPosts)
-)
+// blog.forEach(item => 
+//     item.addEventListener("click", showPosts)
+// )
 function showPosts(){
     posts[this.dataset.number - 1].style.display = "block"
     blog.forEach(item => 
         item.style.display = "none"
     )
 }
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY <= 400) {
+      arrowUp.style.display="none" 
+      arrowDown.style.display="flex"
+      arrows.style.display="none"  
+    } 
+    else if (window.scrollY > 400 && window.scrollY < 800) {
+        arrowUp.style.display="none"
+        arrowDown.style.display="none"
+        arrows.style.display="block"  
+      }
+    else{
+        arrowUp.style.display="flex"  
+        arrowDown.style.display="none"
+        arrows.style.display="none"  
+      }
+  });
