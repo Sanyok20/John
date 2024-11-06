c49f7207700b371aa15c5190b03c9c5b08d97be6
Sanyok20 <sanyok2009kor@gmail.com>
2024-11-06 21:54:34 +0200

    додав preloader

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 //find all objects
+console.clear()
 let works = document.querySelectorAll(".work")
 let nav = document.querySelector(".nav")
 let blog = document.querySelector(".blog")
 let burgerIcon = document.querySelector(".burger-icon")
+let arrows = document.querySelector(".arrows")
 let arrowUp = document.querySelector(".arrow-up")
 let arrowDown = document.querySelector(".arrow-down")
-let arrows = document.querySelector(".arrows")
+console.log(arrows)
 //show menu
 burgerIcon.addEventListener("click", showMenu)
 function showMenu(){
@@ -25,30 +27,39 @@ function showDetails(){
 }
 let posts = document.querySelectorAll(".post")
 console.log(posts)
-// blog.forEach(item => 
-//     item.addEventListener("click", showPosts)
-// )
+
 function showPosts(){
     posts[this.dataset.number - 1].style.display = "block"
     blog.forEach(item => 
         item.style.display = "none"
     )
 }
-
+let scrollHeight = Math.max(
+    document.body.scrollHeight, document.documentElement.scrollHeight,
+    document.body.offsetHeight, document.documentElement.offsetHeight,
+    document.body.clientHeight, document.documentElement.clientHeight
+  );
 window.addEventListener("scroll", () => {
-    if (window.scrollY <= 400) {
-      arrowUp.style.display="none" 
-      arrowDown.style.display="flex"
-      arrows.style.display="none"  
+    if (window.scrollY <= scrollHeight * 0.27) {
+       arrowUp.style.display="none" 
+       arrowDown.style.display="flex"
+       arrows.style.display="none"  
     } 
-    else if (window.scrollY > 600 && window.scrollY < 800) {
+    else if (window.scrollY > scrollHeight * 0.27 && window.scrollY < scrollHeight * 0.4) {
         arrowUp.style.display="none"
         arrowDown.style.display="none"
         arrows.style.display="block"  
-      }
+    }
     else{
         arrowUp.style.display="flex"  
         arrowDown.style.display="none"
         arrows.style.display="none"  
-      }
-  });
+    }
+   });
+window.onload = function() {
+    let preloader = document.getElementById('preloader');
+    preloader.classList.add('hide-preloader');
+    setInterval(function() {
+        preloader.classList.add('preloader-hidden');
+    }, 990);
+}
